fccb21fbc2691b9a14bb48c051456604d4039fcd
Nadav Mailhot <mailhotn@gmail.com>
2018-04-15 16:06:47 +0300

    Updated fg_lagrange to use N-R Instead of Solve
    Improved run-time by about x80

--- StumS.m
+++ StumS.m
@@ -1,14 +1,8 @@
-function Sz = StumS(z,a)
+function Sz = StumS(z)
 % StumS calculates the S Stumpff function of input z
-% In order for this to work for symbolic inputs when solving the universal
-% Kepler equation, the sign is taken from alpha = 1/a, the inverse of the
-% semimajor axis, determining the type of orbit
-if nargin == 1
-    a = z;
-end
-if a > 0
+if z > 0
     Sz = (sqrt(z)-sin(sqrt(z)))/sqrt(z)^3;
-elseif a < 0
+elseif z < 0
     Sz = (sinh(sqrt(-z))-sqrt(-z))/sqrt(-z)^3;
 else
     Sz = 1/6;
--- fg_lagrange.m
+++ fg_lagrange.m
@@ -1,7 +1,8 @@
-function [ r, v ] = fg_lagrange( r0, v0, dt, mu )
+function [ R, V ] = fg_lagrange( R0, V0, dt, tol, mu )
 %fg_lagrange calculates change of position & velocity over time interval
 %   calculates lagrange coefficients f, g, f_dot, g_dot and applies them to
 %   the initial state to get the new state after dt
+%   Solves for universal variable X using Newton-Raphson method.
 %
 %  ~~~~~~~~~~   Variables    ~~~~~~~~~~~
 % mu - gravitational parameter (km^3/s^2)
@@ -10,22 +11,31 @@ function [ r, v ] = fg_lagrange( r0, v0, dt, mu )
 % r  - the final position vector (km)
 % v  - the final velocity vector (km/s)
 % dt - elapsed time (s)
-
 if nargin < 4
+    tol = 1e-14;
+end
+if nargin < 5
     mu = 398600; % default is earth orbit of satellite with negligible mass
 end
-syms X;
-r0m = norm(r0);
-v0m = norm(v0);
-vr0 = dot(v0,r0)/r0m;
-a = 2/r0m - v0m^2/mu; % inverse of semimajor axis
-eq = sqrt(mu)*dt == (r0m*vr0/sqrt(mu))*X^2*StumC(a*X^2,a) + (1-a*r0m)*X^3*StumS(a*X^2,a) + r0m*X; %#ok
-X = double(vpasolve(eq,X,sqrt(mu)*abs(a)*dt));
-f = 1 - X^2/r0m*StumC(a*X^2);
+r0 = norm(R0);
+v0 = norm(V0);
+vr0 = dot(V0,R0)/r0;
+a = 2/r0 - v0^2/mu; % inverse of semimajor axis
+X = sqrt(mu)*abs(a)*dt;
+ratio = inf;
+while ratio > tol
+    f = (r0*vr0/sqrt(mu))*X^2*StumC(a*X^2) +...
+        (1-a*r0)*X^3*StumS(a*X^2) + r0*X - sqrt(mu)*dt;
+    df = (r0*vr0/sqrt(mu))*X*(1 - a*X^2*StumS(a*X^2)) +...
+        (1 - a*r0)*X^2*StumC(a*X^2) + r0;
+    ratio = f/df;
+    X = X - ratio;
+end
+f = 1 - X^2/r0*StumC(a*X^2);
 g = dt - 1/sqrt(mu)*X^3*StumS(a*X^2);
-r = f*r0 + g*v0;
-rm = norm(r);
-df = sqrt(mu)/(rm*r0m)*(a*X^3*StumS(a*X^2)-X);
+R = f*R0 + g*V0;
+rm = norm(R);
+df = sqrt(mu)/(rm*r0)*(a*X^3*StumS(a*X^2)-X);
 dg = 1 - X^2/rm*StumC(a*X^2);
-v = df*r0 + dg*v0;
+V = df*R0 + dg*V0;
 end
