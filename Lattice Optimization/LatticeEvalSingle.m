7bf5c052068cb26d3580f621c58ad5d1a8905835
Nadav Mailhot <mailhotn@gmail.com>
2018-08-19 11:20:15 +0300

    Lattice Evaluation Changes

--- Lattice Optimization/LatticeEvalSingle.m
+++ Lattice Optimization/LatticeEvalSingle.m
@@ -1,7 +1,10 @@
 
-datafolder = 'C:\Users\User\Dropbox\Lattice Optimization Data';
+datafolder = 'C:\Users\User\Dropbox\Lattice Optimization Data\Previous Runs\Lattice Version 1';
+walkerFolder = ['C:\Users\User\Dropbox\Walker Optimization Data'...
+    '\Previous Optimization Runs\Walker RGT Ex Search delta inc 10'];
 %% Choose Constellation
-latGs = 40;
+latGs = 50;
+lonGs = 0;
 ecc = 0.05;
 nSats = 51;
 nPlanes = 51;
@@ -30,21 +33,96 @@ end
 Phase.nC2 = nC2;
 
 LC = LatticeConstellation(Arch,Phase,ExSol.Orbit,ExSol.InitCon);
+nSatsW = 64;
+load([walkerFolder '\WalkerRgtExSol_Lat_' num2str(latGs)...
+                '_T_' num2str(nSatsW) '.mat']);
+WC = WalkerConstellation(64,8,2,ExSol.inc,ExSol.alt,ExSol.raan0);
 %% Propagate
 Prop = Propagator(LC,PropParams.relTol,PropParams.absTol);
+PropW = Propagator(WC,PropParams.relTol,PropParams.absTol);
 [propTime, propState] = Prop.PropEciJ2(PropParams.timeVec);
+[propTimeW, propStateW] = PropW.PropEciJ2(PropParams.timeVec);
 % Evaluate PDOP
 [pdop, satsIs] = TdoaPdopVec(propState,propTime,latGs,0,0,PropParams.elevMin);
 %% Plot Stuff
 figure(1)
 PlotGroundTrack(propState,propTime,0)
 
-figure(2)
-yyaxis left
-plot(propTime/3600,pdop)
-ylabel('PDOP')
-yyaxis right
-plot(propTime/3600,satsIs)
-ylabel('Sats In Sight')
-xlabel('Time [hr]')
-grid
+% figure(2)
+% yyaxis left
+% plot(propTime/3600,pdop)
+% ylabel('PDOP')
+% yyaxis right
+% plot(propTime/3600,satsIs)
+% ylabel('Sats In Sight')
+% xlabel('Time [hr]')
+% grid
+%% Plot PDOP Map
+lats = 40:1:60;
+lons = -20:1:20;
+[LON,LAT] = meshgrid(lons,lats);
+PDOP = nan(size(LON));
+PDOPW = nan(size(LON));
+tic
+parfor iLon = 1:length(lons)
+    PDOP2 = zeros(length(lats),1);
+    PDOPW2 = zeros(length(lats),1);
+    for iLat = 1:length(lats)
+        pdop = TdoaPdopVec(propState,propTime,LAT(iLat,iLon),LON(iLat,iLon)...
+            ,0,PropParams.elevMin);
+        pdop(pdop>100) = 100;
+        pdop(isnan(pdop)) = 100;
+        PDOP2(iLat) = mean(pdop(~isnan(pdop)));
+        
+        pdopW = TdoaPdopVec(propStateW,propTimeW,LAT(iLat,iLon),LON(iLat,iLon)...
+            ,0,PropParams.elevMin);
+        pdopW(pdopW>100) = 100;
+        pdopW(isnan(pdopW)) = 100;
+        PDOPW2(iLat) = mean(pdopW(~isnan(pdopW)));
+    end
+    PDOP(:,iLon) =PDOP2;
+    PDOPW(:,iLon) =PDOPW2;
+end
+toc
+figure(3)
+subplot(2,1,1)
+
+contourf(LON,LAT,PDOP,2000,'LineColor','none')
+title('Lattice Flower Constellation 51/51')
+hold on
+
+colormap jet
+shading interp
+c = colorbar;
+c.Label.String = 'Mean PDOP';
+xlabel('Longitude')
+ylabel('Latitude')
+load coastlines
+geoshow(coastlat,coastlon)
+plot(lonGs,latGs,'y*','LineWidth',1.5)
+axis equal
+xlim([-30,30])
+ylim([30,70])
+grid on
+hold off
+
+subplot(2,1,2)
+
+contourf(LON,LAT,PDOPW,2000,'LineColor','none')
+title('Walker Constellation 64/8')
+hold on
+
+colormap jet
+shading interp
+c = colorbar;
+c.Label.String = 'Mean PDOP';
+xlabel('Longitude')
+ylabel('Latitude')
+load coastlines
+geoshow(coastlat,coastlon)
+plot(lonGs,latGs,'y*','LineWidth',1.5)
+axis equal
+xlim([-30,30])
+ylim([30,70])
+grid on
+hold off
