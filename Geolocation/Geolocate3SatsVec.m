48ab8471ce53a7ed50711dca9a23c147ff903325
Nadav Mailhot <mailhotn@gmail.com>
2018-09-05 11:44:30 +0300

    Moved File Around

--- Geolocation/Geolocate3SatsVec.m
+++ Geolocation/Geolocate3SatsVec.m
@@ -1,7 +1,7 @@
-function [ output_args ] = Geolocate3SatsVec(xEci, time, latGs, lonGs,...
-                                             elevMin, timeVar)
+function [rGsEst, estError, pdop] = Geolocate3SatsVec(xEci, time, latGs,...
+    lonGs, elevMin, timeVar)
 %Geolocate3SatsVec Estimates the ground station position over time
-%   Detailed explanation goes here
+%   Spherical Earth model, only 3 satellites taken into account
 %% Input Arguments
 % * xEci    - Mx(6N) matrix of ECI states for N satellites at M time steps
 % * time    - Mx1 vector of time steps
@@ -15,11 +15,10 @@ nSats = size(xEci,2)/6;
 gmst = wrapTo360(time*primary.we);
 rGs = primary.Re*[cosd(lonGs)*cosd(latGs); sind(lonGs)*cosd(latGs); sind(latGs)];
 flagVec = nan(nTime,1);
-rGsEst = nan(3,nTime);
-rGsEst2 = nan(3,nTime);
+rGsEst = nan(6,nTime);
 pdop = nan(1,nTime);
-estError = inf(1,nTime);
-estError2 = inf(1,nTime);
+estError = inf(2,nTime);
+
 for iTime = 1:nTime
     xEciNow = reshape(xEci(iTime,:).',6,nSats);
     xEcef = eci2ecef(xEciNow,gmst(iTime));
@@ -28,13 +27,12 @@ for iTime = 1:nTime
     flagVec(iTime) = flagNow;
     pdop(iTime) = pdopNow;
     if flagNow ~= 5
-        rGsEst(:,iTime) = rGsNow;
-        estError(iTime) = norm(rGsNow-rGs);
+        rGsEst(1:3,iTime) = rGsNow;
+        estError(1,iTime) = norm(rGsNow-rGs);
     else
-        rGsEst(:,iTime) = rGsNow(4:6);
-        rGsEst2(:,iTime) = rGsNow(1:3);
-        estError(iTime) = norm(rGsNow(4:6)-rGs);
-        estError2(iTime) = norm(rGsNow(1:3)-rGs);
+        rGsEst(:,iTime) = rGsNow;
+        estError(1,iTime) = norm(rGsNow(1:3)-rGs);
+        estError(2,iTime) = norm(rGsNow(4:6)-rGs);
     end
 end
 end
