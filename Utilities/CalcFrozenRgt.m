64a7bab86a280986c1164b4218a5bfea99706bfb
Nadav Mailhot <mailhotn@gmail.com>
2018-10-15 12:00:31 +0300

    Added Frozen Orbit & Frozen RGT Generation
    + earth uses Vallado numbers
    - need to test frozen orbits with J3 Prop

---
+++ Utilities/CalcFrozenRgt.m
@@ -0,0 +1,71 @@
+function [ sma, ecc, fVal] = CalcFrozenRgt( inc, aop, j, k )
+%CalcFrozenRgt calculates e & a for a frozen RGT Orbit
+%   Uses multivariate Newton - Raphson method
+
+%   ~~~~~~~~~~~~~  INPUTS  ~~~~~~~~~~~~~~
+%   inc - orbital inclination [deg]
+%   aop - argument of perigee [deg]
+%   j   - number of repeats
+%   k   - number of days to repeat
+
+% Handle Input
+inc = inc*pi/180;
+aop = aop*pi/180;
+% Planet
+primary = earth();
+J2 = primary.J2;
+J3 = primary.J3;
+Re = primary.Re;
+mu = primary.mu;
+Tday = primary.Tday;
+we = primary.we;
+
+% N-R intialization
+tol = 1e-12;
+delX = inf(2,1);
+fVal = ones(2,1);
+iter = 0;
+maxIter = 20;
+
+x = [J2;
+    (((k*Tday/j)/2/pi)^2*mu)^(1/3)];
+
+while norm(delX) > tol && iter < maxIter
+    J2Effects = 3/4*J2*Re^2/(1 - x(1)^2)^2 * ...
+        (2*j/k*cos(inc) - sqrt(1 - x(1)^2)*(3*cos(inc)^2 - 1) - (5*cos(inc)^2 - 1));
+    
+    f1 = J2 + J3/2*Re*sin(aop)/x(2)*...
+        1/(x(1) - x(1)^3)*...
+        (sin(inc) - x(1)*cos(inc)^2/sin(inc));
+    
+    f2 = x(2)^2 - j*we*(pi/180)/(k*sqrt(mu))*x(2)^(7/2) - J2Effects;
+    
+    df1de = J3/2*Re*sin(aop)/x(2)*(...
+        (3*x(1)^2 - 1)/(x(1) - x(1)^3)^2*sin(inc) -...
+        2*x(1)/(1 - x(1)^2)^2 *cos(inc)^2/sin(inc));
+    
+    df1da = -J3/2*Re*sin(aop)/x(2)^2*...
+        1/(x(1) - x(1)^3)*...
+        (sin(inc) - x(1)*cos(inc)^2/sin(inc));
+    
+    df2de = -3/4*J2*Re^2*...
+        x(1)/(1 - x(1)^2)^2*...
+        (8*(5*cos(inc)^2 - 1) - 16*j/k*cos(inc) +9/2*(3*cos(inc)^2 - 1));
+    
+    df2da = 2*x(2) - 7*j*we*(pi/180)/(2*k*sqrt(mu))*x(2)^(5/2);
+    
+    J = [df1de df1da;
+        df2de df2da];
+    fVal = [f1; f2];
+    delX = -J\fVal;
+    
+    x = x + delX;
+    iter = iter + 1;
+end
+if x(1) > 0
+    ecc = x(1);
+    sma = x(2);
+else
+    error('No viable solution found!')
+end
+end
--- Utilities/earth.m
+++ Utilities/earth.m
@@ -10,12 +10,9 @@ function earthstruct = earth()
 % we - earth rotation rate (deg/s)
 % Tday - length of sidereal day (s)
 
-earthstruct.mu = 398600.440;
-% earthstruct.mu = 398600.4415;
-earthstruct.Re = 6378.137;
-% earthstruct.Re = 6378.1363;
-earthstruct.J2 = 0.0010826265;
-% earthstruct.J2 = 1.08262617385222e-3;
+earthstruct.mu = 398600.4415;
+earthstruct.Re = 6378.1363;
+earthstruct.J2 = 1.08262617385222e-3;
 earthstruct.J3 = -2.53241051856772e-6;
 earthstruct.we = 180/pi*7.29211585530e-5;
 earthstruct.Tday = 2*pi/7.29211585530e-5;
