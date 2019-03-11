f30308493165dfb98f8edccfc717dc4355f06d0f
Nadav Mailhot <mailhotn@gmail.com>
2019-03-11 16:33:42 +0200

    Added RGT Based on Apogee Height
    + Small error fix in frozen RGT that had no impact on anything.

--- Utilities/CalcRgtSmaApoHeight.m
+++ Utilities/CalcRgtSmaApoHeight.m
@@ -1,24 +1,25 @@
-function [ sma, ecc, fVal] = CalcFrozenRgt( inc, aop, j, k )
-%CalcFrozenRgt calculates e & a for a frozen RGT Orbit
+function [ sma, ecc, fVal] = CalcRgtSmaApoHeight( inc, hA, j, k )
+%CalcRgtSmaApoHeight calculates e & a for an RGT Orbit with given apogee
+%height
 %   Uses multivariate Newton - Raphson method
 
 %   ~~~~~~~~~~~~~  INPUTS  ~~~~~~~~~~~~~~
 %   inc - orbital inclination [deg]
-%   aop - argument of perigee [deg]
+%   hA  - desired apogee height [km]
 %   j   - number of repeats
 %   k   - number of days to repeat
 
-% Handle Input
-inc = inc*pi/180;
-aop = aop*pi/180;
 % Planet
 primary = earth();
 J2 = primary.J2;
-J3 = primary.J3;
 Re = primary.Re;
 mu = primary.mu;
 Tday = primary.Tday;
-we = primary.we*pi/180;
+we = primary.we;
+
+% Handle Input
+inc = inc*pi/180;
+rA = hA + primary.Re;
 
 % N-R intialization
 tol = 1e-12;
@@ -28,35 +29,28 @@ iter = 0;
 maxIter = 20;
 
 initSma = (((k*Tday/j)/2/pi)^2*mu)^(1/3);
-initEcc = -J3/2/J2*Re/initSma*sin(inc);
+initEcc = rA/initSma - 1;
 x = [initEcc;
     initSma];
 
 while norm(delX,inf) > tol && iter < maxIter
     J2Effects = 3/4*J2*Re^2/(1 - x(1)^2)^2 * ...
-        (2*j/k*cos(inc) - sqrt(1 - x(1)^2)*(3*cos(inc)^2 - 1) - ...
-        (5*cos(inc)^2 - 1));
+        (2*j/k*cos(inc) - sqrt(1 - x(1)^2)*(3*cos(inc)^2 - 1) - (5*cos(inc)^2 - 1));
     
-    f1 = J2 + J3/2*Re*sin(aop)/x(2)*...
-        1/(x(1) - x(1)^3)*...
-        (sin(inc) - x(1)*cos(inc)^2/sin(inc));
+    f1 = rA - x(2)*(1 + x(1));
     
-    f2 = x(2)^2 - j*we/(k*sqrt(mu))*x(2)^(7/2) - J2Effects;
+    f2 = x(2)^2 - j*we*(pi/180)/(k*sqrt(mu))*x(2)^(7/2) - J2Effects;
     
-    df1de = J3/2*Re*sin(aop)/x(2)*(...
-        (3*x(1)^2 - 1)/(x(1) - x(1)^3)^2*sin(inc) -...
-        2*x(1)/(1 - x(1)^2)^2 *cos(inc)^2/sin(inc));
+    df1de = -x(2);
     
-    df1da = -J3/2*Re*sin(aop)/x(2)^2*...
-        1/(x(1) - x(1)^3)*...
-        (sin(inc) - x(1)*cos(inc)^2/sin(inc));
+    df1da = -(1 + x(1));
     
     df2de = 3/4*J2*Re^2*...
         x(1)/(1 - x(1)^2)^3*...
         (-4*(5*cos(inc)^2 - 1) + 8*j/k*cos(inc) - sqrt(1-x(1)^2)*...
         3*(3*cos(inc)^2 - 1));
     
-    df2da = 2*x(2) - 7*j*we/(2*k*sqrt(mu))*x(2)^(5/2);
+    df2da = 2*x(2) - 7*j*we*(pi/180)/(2*k*sqrt(mu))*x(2)^(5/2);
     
     J = [df1de df1da;
         df2de df2da];
