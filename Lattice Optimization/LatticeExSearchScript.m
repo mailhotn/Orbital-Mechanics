f07ab000eb836a2e6a321ea6e6f9bfd72f385fae
Nadav Mailhot <mailhotn@gmail.com>
2019-03-24 11:28:59 +0200

    Finalized Apogee Height and Lattice Walker
    + Walker search works

--- Lattice Optimization/LatticeExSearchScript.m
+++ Lattice Optimization/LatticeExSearchScript.m
@@ -11,61 +11,64 @@ primary = earth();
 nRepeats = 14;
 nDays    = 1;
 
-latEm = 40;
+latList = 10:10:80;
+% latEm = 40;
 lonEm = 0;
 maxSats = 80;
-minSats = 45;
+minSats = 30;
 
 hAList = [0, 900, 1000];
 save([PropParams.datafolder '\OptParams.mat']);
 
 %% Perform Search
-
-parfor iHA = 1:length(hAList)
-    Orbit = struct();
-    Orbit.inc = latEm + 10;
-    [sma, ecc] = CalcRgtSmaApoHeight(Orbit.inc, hAList(iHA), nRepeats, nDays);
-    Orbit.sma = sma;
-    Orbit.ecc = ecc;
-    Orbit.hA = hAList(iHA);
-    InitCon = InitConElliptical(Orbit.ecc,Orbit.inc,Orbit.sma,latEm,lonEm);
-    for nSats = minSats:maxSats
-        % Optimize
-        try
-            % Run Search for T & latGs
-            tic
-            Arch = struct();
-            Arch.nSats = nSats;
-            Arch.nRepeats = nRepeats;
-            Arch.nDays = nDays;
-            
-            ExSol = LatticeExSearch(Arch,Orbit,InitCon,latEm,PropParams);
-            optTime = toc;
-            % Verbose Output Message
-            c = clock;
-            disp([newline num2str(c(3)) '/' num2str(c(2)) ' ' num2str(c(4)) ':' num2str(c(5)) ':' num2str(c(6),2)...
-                newline 'Optimization Complete for nSats = ' num2str(nSats) ...
-                ' and Latitude = ' num2str(latEm)...
-                newline 'Fitness: ' num2str(ExSol.fit)...
-                newline 'Elapsed Time: ' num2str((optTime-mod(optTime,60))/60) ' min '...
-                num2str(mod(optTime,60),3) ' sec'...
-                newline 'Optimal Solution nSats/nPlanes/nC1/nC2/nC3: '...
-                num2str(nSats) '/' num2str(ExSol.archMat(1,ExSol.iOpt))...
-                '/' num2str(ExSol.phaseMat(1,ExSol.iOpt)) ...
-                '/' num2str(ExSol.phaseMat(2,ExSol.iOpt)) ...
-                '/' num2str(ExSol.phaseMat(3,ExSol.iOpt)) ...
-                newline 'Inclination: ' num2str(Orbit.inc) '°'...
-                newline 'Eccentricity: ' num2str(Orbit.ecc)...
-                newline 'Semimajor Axis: ' num2str(Orbit.sma) ' km' ...
-                newline 'Earth Repeats: ' num2str(nRepeats)])
-        catch ME
-            % Error!
-            c = clock;
-            fileID = fopen(['C:\Users\User\Dropbox\zzzMatlab Errors\error_'...
-                num2str(c(3)) '-' num2str(c(2)) '_' num2str(c(4)) ...
-                '.' num2str(c(5)) '.txt'],'w'); % create error file for IFTT phone notification
-            fprintf(fileID,ME.message);
-            rethrow(ME)
+parfor iLat = 1:length(latList)
+    latEm = latList(iLat)
+    for iHA = 1:length(hAList)
+        Orbit = struct();
+        Orbit.inc = min([89,latEm + 10]);
+        [sma, ecc] = CalcRgtSmaApoHeight(Orbit.inc, hAList(iHA), nRepeats, nDays);
+        Orbit.sma = sma;
+        Orbit.ecc = ecc;
+        Orbit.hA = hAList(iHA);
+        InitCon = InitConElliptical(Orbit.ecc,Orbit.inc,Orbit.sma,latEm,lonEm);
+        for nSats = minSats:maxSats
+            % Optimize
+            try
+                % Run Search for T & latGs
+                tic
+                Arch = struct();
+                Arch.nSats = nSats;
+                Arch.nRepeats = nRepeats;
+                Arch.nDays = nDays;
+                
+                ExSol = LatticeExSearch(Arch,Orbit,InitCon,latEm,PropParams);
+                optTime = toc;
+                % Verbose Output Message
+                c = clock;
+                disp([newline num2str(c(3)) '/' num2str(c(2)) ' ' num2str(c(4)) ':' num2str(c(5)) ':' num2str(c(6),2)...
+                    newline 'Optimization Complete for nSats = ' num2str(nSats) ...
+                    ' and Latitude = ' num2str(latEm)...
+                    newline 'Fitness: ' num2str(ExSol.fit)...
+                    newline 'Elapsed Time: ' num2str((optTime-mod(optTime,60))/60) ' min '...
+                    num2str(mod(optTime,60),3) ' sec'...
+                    newline 'Optimal Solution nSats/nPlanes/nC1/nC2/nC3: '...
+                    num2str(nSats) '/' num2str(ExSol.archMat(1,ExSol.iOpt))...
+                    '/' num2str(ExSol.phaseMat(1,ExSol.iOpt)) ...
+                    '/' num2str(ExSol.phaseMat(2,ExSol.iOpt)) ...
+                    '/' num2str(ExSol.phaseMat(3,ExSol.iOpt)) ...
+                    newline 'Inclination: ' num2str(Orbit.inc) '°'...
+                    newline 'Eccentricity: ' num2str(Orbit.ecc)...
+                    newline 'Semimajor Axis: ' num2str(Orbit.sma) ' km' ...
+                    newline 'Earth Repeats: ' num2str(nRepeats)])
+            catch ME
+                % Error!
+                c = clock;
+                fileID = fopen(['C:\Users\User\Dropbox\zzzMatlab Errors\error_'...
+                    num2str(c(3)) '-' num2str(c(2)) '_' num2str(c(4)) ...
+                    '.' num2str(c(5)) '.txt'],'w'); % create error file for IFTT phone notification
+                fprintf(fileID,ME.message);
+                rethrow(ME)
+            end
         end
     end
 end
--- Walker Optimization/WalkerExSearchScript.m
+++ Walker Optimization/WalkerExSearchScript.m
@@ -1,68 +1,69 @@
 %% Initialization
+clear
 PropParams.maxPdop = 1000;
 PropParams.timeVec = 0:10:86164;
-lonEm = 0;
 PropParams.elevMin  = 5;
 PropParams.relTol = 1e-6;
 PropParams.absTol = 1e-6;
 PropParams.datafolder = 'C:\Users\User\Dropbox\Walker Optimization Data';
-
-latEm = 40;
-Rgt.jRepeats = 14;
-Rgt.kDays    = 1;
 primary = earth();
-nOrbits = 6;
 
+nRepeats = 14;
+nDays    = 1;
+
+latList = 10:10:80;
+% latEm = 40;
+lonEm = 0;
 maxSats = 80;
-minSats = 45;
+minSats = 30;
 
-delInc = 10;
+save([PropParams.datafolder '\OptParams.mat']);
 
-incList = [5,10,20,25];
-
-% save([PropParams.datafolder '\OptParams.mat']);
 %% Massive For Loop
 
-
-GroundS = struct();
-GroundS.lat = latEm;
-GroundS.lon = 0;
-ecc = 0;
-%     [inc, sma, raan0] = VirtualSatOe(ecc,Rgt,GroundS,nOrbits);
-inc = min([90,latEm + delInc]);
-sma = CalcRgtSma(ecc,inc,Rgt.jRepeats,Rgt.kDays);
-
-InitCon = InitConElliptical(0,inc,sma,latEm,lonEm);
-raan0 = InitCon.raan1;
-parfor nSats = minSats:maxSats
-    % Optimize
-    try
-        % Run Search for T & latGs
-        tic
-        ExSol = WalkerExSearch(nSats, inc, sma, raan0, latEm, PropParams);
-        optTime = toc;
-        % Verbose Output Message
-        c = clock;
-        disp([newline num2str(c(3)) '/' num2str(c(2)) ' ' num2str(c(4)) ':' num2str(c(5)) ':' num2str(c(6),2)...
-            newline 'Optimization Complete for T = ' num2str(nSats) ...
-            ' and Latitude = ' num2str(latEm)...
-            newline 'Fitness: ' num2str(ExSol.fit)...
-            newline 'Elapsed Time: ' num2str((optTime-mod(optTime,60))/60) ' min '...
-            num2str(mod(optTime,60),3) ' sec'...
-            newline 'Optimal Solution i:T/P/F: '...
-            num2str(inc) '°'  ':' num2str(nSats) '/' num2str(ExSol.optP)...
-            '/' num2str(ExSol.optF) ...
-            newline 'Semimajor Axis: ' num2str(sma) ' km' ...
-            newline 'Earth Repeats: ' num2str(Rgt.jRepeats)])
-    catch ME
-        % Error!
-        c = clock;
-        fileID = fopen(['C:\Users\User\Dropbox\zzzMatlab Errors\error_'...
-            num2str(c(3)) '-' num2str(c(2)) '_' num2str(c(4)) ...
-            '.' num2str(c(5)) '.txt'],'w'); % create error file for IFTT phone notification
-        fprintf(fileID,ME.message);
-        rethrow(ME)
-        
+parfor iLat = 1:length(latList)
+    latEm = latList(iLat);
+    Orbit = struct();
+    Orbit.ecc = 0;
+    Orbit.inc = min([89,latEm + 10]);
+    Orbit.sma = CalcRgtSma(Orbit.ecc,Orbit.inc,nRepeats,nDays);
+    Orbit.hA = 0;
+    
+    InitCon = InitConElliptical(Orbit.ecc,Orbit.inc,Orbit.sma,latEm,lonEm);
+    for nSats = minSats:maxSats
+        % Optimize
+        try
+            % Run Search for T & latGs
+            tic
+            Arch = struct();
+            Arch.nSats = nSats;
+            Arch.nRepeats = nRepeats;
+            Arch.nDays = nDays;
+            ExSol = WalkerExSearch( Arch, Orbit, InitCon, latEm, PropParams);
+            optTime = toc;
+            % Verbose Output Message
+            c = clock;
+            disp([newline num2str(c(3)) '/' num2str(c(2)) ' ' num2str(c(4)) ':' num2str(c(5)) ':' num2str(c(6),2)...
+                newline 'Optimization Complete for T = ' num2str(nSats) ...
+                ' and Latitude = ' num2str(latEm)...
+                newline 'Fitness: ' num2str(ExSol.fit)...
+                newline 'Elapsed Time: ' num2str((optTime-mod(optTime,60))/60) ' min '...
+                num2str(mod(optTime,60),3) ' sec'...
+                newline 'Optimal Solution i:T/P/F: '...
+                num2str(Orbit.inc) '°'  ':' num2str(nSats) '/' num2str(ExSol.optNPlanes)...
+                '/' num2str(-ExSol.phaseMat(1,ExSol.iOpt)) ...
+                newline 'Semimajor Axis: ' num2str(Orbit.sma) ' km' ...
+                newline 'Earth Repeats: ' num2str(nRepeats)])
+        catch ME
+            % Error!
+            c = clock;
+            fileID = fopen(['C:\Users\User\Dropbox\zzzMatlab Errors\error_'...
+                num2str(c(3)) '-' num2str(c(2)) '_' num2str(c(4)) ...
+                '.' num2str(c(5)) '.txt'],'w'); % create error file for IFTT phone notification
+            fprintf(fileID,ME.message);
+            rethrow(ME)
+            
+        end
     end
 end
 
