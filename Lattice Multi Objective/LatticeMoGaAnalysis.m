de2563606f20c31187e4532ed45c13394bc93a7b
Nadav Mailhot <mailhotn@gmail.com>
2019-07-11 11:57:50 +0300

    Cuttoff PDOP fitness at 100
    - Should give less redundant pareto solutions

---
+++ Lattice Multi Objective/LatticeMoGaAnalysis.m
@@ -0,0 +1,28 @@
+datafolder = 'C:\Users\User\Dropbox\Lattice Optimization Data\Multi Objective';
+intTarget = 10;
+p90Target = 5;
+covTarget = 99;
+maxTarget = 10;
+latList = 30:10:60;
+%% Loop
+for iLat = 1:numel(latList)
+    load([datafolder '\LatticeGaSol_Lat_' num2str(latList(iLat)) '.mat'])
+    achieveInt = mean(GaSol.intPdop,1) < intTarget;
+    achievep90 = mean(GaSol.p90,1) < p90Target;
+    achieveCov = mean(GaSol.coverage,1) > covTarget;
+    achieveMax = mean(GaSol.maxPdop,1) < maxTarget;
+    achieveAll = achieveInt & achievep90 & achieveCov & achieveMax;
+    conList = find(achieveAll);
+    
+    parSats = GaSol.Cons{conList(1)}.nSats;
+    parPlanes = GaSol.Cons{conList(1)}.nPlanes;
+    for iCon = 2:numel(conList) % ascending nSats
+        if GaSol.Cons{conList(iCon)}.nPlanes < min(parPlanes)
+            parSats = [parSats GaSol.Cons{conList(iCon)}.nSats];
+            parPlanes = [parPlanes GaSol.Cons{conList(iCon)}.nPlanes];
+        end
+    end
+    figure(10*iLat + 1)
+    plot(parSats,parPlanes,'--o')
+end
+
