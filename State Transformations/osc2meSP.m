7a4c7d605c934adeddc9a727b3c4bdec8cf53345
Unknown <mailhotn@gmail.com>
2022-08-30 13:09:32 +0300

    Short Period Transforms
    + Test propagation & stuff
    - what's up with the mean rates?

--- State Transformations/osc2meSP.m
+++ State Transformations/osc2meSP.m
@@ -1,5 +1,5 @@
-function [OE_osc] = me2oscSP(OE_m,primary)
-%me2oscSP Calculates the osculating orbital elements of a satellite
+function [oeM] = osc2meSP(oeOsc,primary)
+%osc2meSP Calculates the osculating orbital elements of a satellite
 %   Based on Kozai 1959
 %   Accepts a 6xN Matrix of osculating elements in the following order:
 %
@@ -19,17 +19,17 @@ end
 J2 = primary.J2;
 Re = primary.Re;
 
-sma0 = OE_m(1,:);
-ecc0 = OE_m(2,:);
-inc0 = pi/180*OE_m(3,:);
-ran0 = wrapTo2Pi(pi/180*OE_m(4,:));
-aop0 = wrapTo2Pi(pi/180*OE_m(5,:));
-man0 = wrapTo360(OE_m(6,:));
+sma0 = oeOsc(1,:);
+ecc0 = oeOsc(2,:);
+inc0 = pi/180*oeOsc(3,:);
+ran0 = wrapTo2Pi(pi/180*oeOsc(4,:));
+aop0 = wrapTo2Pi(pi/180*oeOsc(5,:));
+man0 = wrapTo360(oeOsc(6,:));
 f = me2ta(man0,ecc0);
 man0 = pi/180*man0;
 f = pi/180*f;
 
-A2 = J2*Re^2*1.5; % positive A2: Me 2 Osc
+A2 = -J2*Re^2*1.5; % positive A2: osc2me
 
 % sma0 = sma0.*(1-A2./(sma0.*(1-ecc0.^2)).^2.*(1-1.5*sin(inc0).^2).*eta); % Idk
 
@@ -87,4 +87,4 @@ ran1 = ran0 + dRanS - dRanM;
 aop1 = aop0 + dAopS - dAopM;
 man1 = man0 + dManS - dManM;
 
-OE_osc = [sma1;ecc1;inc1;ran1;aop1;man1];
+oeM = [sma1;ecc1;inc1;ran1;aop1;man1];
