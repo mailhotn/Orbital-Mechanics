ddafe340a9c8c0e0db2757a4dc43c33b241fb193
Nadav Mailhot <mailhotn@gmail.com>
2024-02-14 14:39:20 +0200

    Progress on me2oscNum
    - doesn't work

--- State Transformations/me2oscNum.m
+++ State Transformations/me2oscNum.m
@@ -1,11 +1,20 @@
-function [oeOsc1] = me2oscNum(oeM)
+function [oeOsc1,fVal] = me2oscNum(oeM)
 %me2oscNum finds the oscullating elements at time 0, that result in the
 % desired mean elements, based on a simulation from time -T to time 0
 
 % Convoluted way of saying this is a numerical mean to Oscullating
 % transformation
+primary = earth();
+boundVec = oeM*primary.J2;
+boundVec(2) = primary.J2;
+ub = oeM +  boundVec;
+lb = oeM - boundVec;
+lb(2:3) = 0;
+[oeOsc0,fVal] = fmincon(@(x)meanSim(x,oeM),oeM,[],[],[],[],lb,ub);
+oeOsc1 = oeOsc0;
+% Sat = SingleSat(oeOsc0,primary);
+% Prop = Propagator(Sat);
 
-[oeOsc0,fVal] = fmincon(@(x)meanSim(x,oeM),oeM)
 
 % oeOsc1 = oeOsc0 + one Orbit
 
@@ -23,5 +32,9 @@ Sat = SingleSat(x,primary);
 Prop = Propagator(Sat);
 [tVec,xOsc] = Prop.PropOeOsc3(tSpan);
 M0 = xOsc(:,6) - 180/pi*tVec.*sqrt(primary.mu./xOsc(:,1).^3);
-xM = trapz(tVec,[xOsc(:,1:5),M0])/tN; % How to Average Mean anomaly???
+xM = trapz(tVec,[xOsc(:,1:5),M0])/(tVec(end)-tVec(1)); % How to Average Mean anomaly???
+
+unitVec = [1/primary.Re,1,ones(1,4)*pi/180]; 
+oeError = ((xM-oeM.').*unitVec)*((xM.'-oeM).*unitVec.')*1000; 
+
 end
